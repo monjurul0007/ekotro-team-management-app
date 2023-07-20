b954de8bdf5b8194b8554ac44689924cf2c763fd
Murad <muradul.15@gmail.com>
2023-07-20 16:18:44 +0600

    ek-7-backend changed the name and pass to default

--- backend/ekotro_core/settings.py
+++ backend/ekotro_core/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "mydatabase",
         "USER": "mydatabaseuser",
-        "PASSWORD": "root123",
+        "PASSWORD": "mypassword",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
